ae103855c2a27811ec0a47e6341563488cfc007e
Dyschezia <rotem.krispil@mail.huji.ac.il>
2024-10-04 12:34:49 +0200

    Function setting photodiode rect

---
+++ functions/setupPhotodiode.m
@@ -0,0 +1,20 @@
+function Experiment = setupPhotodiode(Experiment)
+    % Currently hardcoded 20pixel big square at the top left corner of the
+    % screen. 
+    
+    % Size of square in pixels
+    size = 20; 
+    
+    % set rectangle
+    photodiodeRect = SetRect(0,0,size,size);
+    
+    % Set color
+    color = [255, 255, 255];
+    
+    % Save
+    Experiment.Photodiode.size = size;
+    Experiment.Photodiode.color = color;
+    Experiment.Photodiode.rect = photodiodeRect;
+    
+    
+    
